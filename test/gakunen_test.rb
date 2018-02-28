77006ca6f28720a683ff190748fa02132c752812
nao taka <naopontan@gmail.com>
2018-02-28 15:34:04 +0900

    テスト通ってた

--- test/gakunen_test.rb
+++ test/gakunen_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class GakunenTest < Minitest::Test
 
   def setup
-    @leo  = Date.civil(2004, 3, 23)
-    @haru = Date.civil(2006, 9, 15)
-    @rika = Date.civil(2009, 2, 24)
+    @leo  = Date.civil(2004, 3, 24)
+    @haru = Date.civil(2006, 9, 16)
+    @rika = Date.civil(2009, 2, 25)
   end
 
   def test_that_it_has_a_version_number
@@ -13,8 +13,8 @@ class GakunenTest < Minitest::Test
   end
 
   def test_sample_case
-    assert_equal '小3', Gakunen.gakunen(@rika, Date.civil(2018, 2, 26)), 'リカ'
-    assert_equal '小5', Gakunen.gakunen(@haru, Date.civil(2018, 2, 26)), 'ハル'
+    assert_equal '小3', Gakunen.gakunen(@rika, Date.civil(2018, 2, 26))
+    assert_equal '小5', Gakunen.gakunen(@haru, Date.civil(2018, 2, 26))
   end
 
   # http://www.mext.go.jp/a_menu/shotou/shugaku/detail/1309966.htm
@@ -22,7 +22,7 @@ class GakunenTest < Minitest::Test
   #  - 学年 → 4/1 に始まり翌年の 3/31 まで
   #  - 一学年は 4/2 生まれ～翌年の 4/1 生まれまでの子で構成される
   def test_cornercase
-    assert_equal '小3', Gakunen.gakunen(@rika, Date.civil(2018, 2, 28)), 'リカ'
+    assert_equal '小3', Gakunen.gakunen(@rika, Date.civil(2018, 2, 28))
   end
 
   def test_age
@@ -39,25 +39,8 @@ class GakunenTest < Minitest::Test
 
   def test_list
     # 1999/4/2 ～ 2000/4/1 が 2006に入学
-#    assert_equal '小1', Gakunen.gakunen(Date.civil(1999, 4, 2), Date.civil(2006, 4, 1)), '7才で一年生'
-#    assert_equal '小1', Gakunen.gakunen(Date.civil(2000, 4, 1), Date.civil(2006, 4, 1)), '6才で一年生. 早生まれ'
+    assert_equal '小1', Gakunen.gakunen(Date.civil(1999, 4, 2), Date.civil(2006, 4, 1)), '7才で一年生'
+    assert_equal '小1', Gakunen.gakunen(Date.civil(2000, 4, 1), Date.civil(2006, 4, 1)), '6才で一年生. 早生まれ'
   end
 
 end
-__END__
-1999/4/2  満6才は 2005/4/2  入学は 2006/4/1
-1999/12/31  満6才は 2005/12/31  入学は 2006/4/1
-
-2000/1/1  満6才は 2006/1/1  入学は 2006/4/1
-2000/3/31 満6才は 2006/3/31 入学は 2006/4/1
-2000/4/1  満6才は 2006/4/1  入学は 2006/4/1
-
-2000/4/2  満6才は 2006/4/2  入学は 2007/4/1
-2000/12/31  満6才は 2006/12/31  入学は 2007/4/1
-
-
-----
-1999/4/2 ～ 2000/4/1 が 2006に入学
-2005/4/2 ～ 2006/4/1 に満6才が入学
-
-すなわち、4/2 時点で満6才なら小1
